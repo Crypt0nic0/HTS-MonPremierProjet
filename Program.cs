880fbbb15bcaee729e4f7e67e49600dd07135cf1
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-11 11:47:04 +0200

    Exercice 2 finished

--- Program.cs
+++ Program.cs
@@ -10,12 +10,23 @@ Console.WriteLine("Utilisateur : " + nom);
 Console.WriteLine("Age : " + age);
 Console.WriteLine((estVivant) ? "Utilisateur vivant" : "Utilisateur HS");
 
-// Exercice affectation variable
-Console.WriteLine("Veuillez saisir votre nom : ");
-string nomUser = Console.ReadLine();
-Console.WriteLine("Bonjour " + nomUser);
+//======= EXERCICE 1 ====== affectation variable
+//Console.WriteLine("Veuillez saisir votre nom : ");
+//string nomUser = Console.ReadLine();
+//Console.WriteLine("Bonjour " + nomUser);
 
 
 // Soom sur les types textuels
 char copyright = '\u00A9';
 Console.WriteLine(copyright);
+
+// Le @ permet de définir les " et \ en tant que texte
+string cheminWindowsSimple = @"C:\docs\fichier.txt";
+
+
+//======== EXERCICE 2 ========
+Console.WriteLine("Saisir un nom : ");
+nom = Console.ReadLine();
+Console.WriteLine("Saisir un âge : ");
+age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Bonjour {nom}, vous avez {age} ans.");
