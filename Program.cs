ca2c8c320b1d1384ce4c21aa2809caf37f984c39
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-11 11:40:14 +0200

    difference between string and char

--- Program.cs
+++ Program.cs
@@ -9,3 +9,13 @@ bool estVivant = true;
 Console.WriteLine("Utilisateur : " + nom);
 Console.WriteLine("Age : " + age);
 Console.WriteLine((estVivant) ? "Utilisateur vivant" : "Utilisateur HS");
+
+// Exercice affectation variable
+Console.WriteLine("Veuillez saisir votre nom : ");
+string nomUser = Console.ReadLine();
+Console.WriteLine("Bonjour " + nomUser);
+
+
+// Soom sur les types textuels
+char copyright = '\u00A9';
+Console.WriteLine(copyright);
