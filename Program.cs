c012e725dfe91edf37d48214cd5698b63d158599
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-11 10:46:50 +0200

    upload perso : test ternaire

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("J'apprend le C# !");
+
+// Différents types de variables
+int age = 33;
+string nom = "Nicolas";
+bool estVivant = true;
+Console.WriteLine("Utilisateur : " + nom);
+Console.WriteLine("Age : " + age);
+Console.WriteLine((estVivant) ? "Utilisateur vivant" : "Utilisateur HS");
